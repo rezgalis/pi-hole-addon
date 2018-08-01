4130f4f7f100373d7fc7b74d8f9d4c76c39c012a
Kaspars <rezgalis.kaspars@gmail.com>
2018-08-01 11:19:01 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#this script is called from command-line to trigger installation of https://github.com/rezgalis/pi-hole-addon repo
+
+#install pre-requisites
+apt-get install git python-pip -y
+pip install gitpython
+
+#clone git repository and remov unneed files
+cd /home/pi
+rm pi-hole-addon/ -R
+git clone https://github.com/rezgalis/pi-hole-addon.git
+cd pi-hole-addon/
+mkdir repo
+rm .git/ -R
+rm blacklists/*
+rm daily-job.py
+
+#chmod cron scripts
+chmod +x dailycron.sh
+chmod +x hourlycron.sh
+
+#install crontab
+(crontab -l ; echo "@hourly sudo /home/pi/pi-hole-addon/hourlycron.sh >> /home/pi/pi-hole-addon/logs/hourlycron.log 2>&1") | sort - | uniq - | crontab -
+(crontab -l ; echo "@midnight sudo /home/pi/pi-hole-addon/dailycron.sh >> /home/pi/pi-hole-addon/logs/dailycron.log 2>&1") | sort - | uniq - | crontab -
+
+#trigger python job this time
+./hourlycron.sh
