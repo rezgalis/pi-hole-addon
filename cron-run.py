6984a62df344b663dfe9d8b8b3ba82e5f42ffe3f
Kaspars <rezgalis.kaspars@gmail.com>
2018-08-01 10:05:19 +0300

    Create cron-run.py

---
+++ cron-run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+import sys, os, hashlib
+
+
+
+
+if __name__== "__main__":
+	
+
+#this job is run at midnight every day
+#main purpose of this script is to compare local daily-job.py and one found in repo/ folder (which is latest version, from github); if those are different, local file is overwritten and then executed
