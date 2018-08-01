aa420d63a58c5f783f44191b0eb8af510a931a10
Kaspars <rezgalis.kaspars@gmail.com>
2018-08-01 12:18:00 +0300

    Update daily-job.py

--- daily-job.py
+++ daily-job.py
@@ -6,6 +6,7 @@ import sys, os, datetime, hashlib, subprocess, shutil
 
 if __name__== "__main__":
 	subprocess.call(["touch", "hello-new.txt"])
+	subprocess.call(["cp", "repo/repo-refresh.py", "repo-refresh.py"])
 
 	
 # this job is triggered via cron-run.py only if local daily-job.py is different from daily-job.py in repo (and, if so, cron-run.py copies new contents and then executes this script)
