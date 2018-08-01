5fe50f1336f14baa652ad8c588d3722b902df5b9
Kaspars <rezgalis.kaspars@gmail.com>
2018-08-01 10:19:31 +0300

    Update daily-job.py

--- daily-job.py
+++ daily-job.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python2
 
-import sys, os, hashlib
+import sys, os, hashlib, subprocess, shutil
 
 
 
 
 if __name__== "__main__":
-
+	subprocess.call(["touch", "/home/pi/hello.txt"])
 # this job is triggered via cron-run.py only if local daily-job.py is different from daily-job.py in repo (and, if so, cron-run.py copies new contents and then executes this script)
 #this script is intended to trigger any extra updates needed (e.g. apt-get update && apt-get upgrade)
